5e2e1d590b3ce8b6e444afad9db5e0c37a949f04
José Valim <jose.valim@plataformatec.com.br>
2013-11-15 11:57:02 +0100

    A plug is simply a function, there is no behaviour

--- lib/plug.ex
+++ lib/plug.ex
@@ -2,29 +2,16 @@ defmodule Plug do
   @moduledoc """
   Specification for a plug.
 
-  A plug is any Elixir module that defines the function `plug/2`.
-  This function receives a `Plug.Conn` as argument and must return
-  a `Plug.Conn`. The second argument is a set of options that must
-  be used by plugs.
+  A plug is any Elixir function that receives two arguments: a
+  `Plug.Conn` record and a set of keywords options.
 
   There is a second kind of plugs called wrappers. They work the
   same as regular plugs except that they are expected to define
-  a `plug/3` function, where the third argument is a function that
-  receives a `Plug.Conn` and returns a `Plug.Conn`. The specification
-  for the wrapper plug can be found in `Plug.Wrapper`.
+  a function with three arguments, where the third argument is a
+  function that receives a `Plug.Conn` record and returns a
+  `Plug.Conn`.
+
+  When defined over modules, it is common for those functions to
+  be named `plug/2` and `plug/3`.
   """
-
-  use Behaviour
-
-  defcallback plug(Plug.Conn.t, Keyword.t) :: Plug.Conn.t
-
-  defmodule Wrapper do
-    @moduledoc """
-    A wrapper plug. See `Plug` for more info.
-    """
-
-    use Behaviour
-
-    defcallback plug(Plug.Conn.t, Keyword.t, (Plug.Conn.t -> Plug.Conn.t)) :: Plug.Conn.t
-  end
 end
