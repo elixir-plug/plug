8a4266c7e5dc7f7ada7912b73dbe78a8eb9966fa
José Valim <jose.valim@plataformatec.com.br>
2014-05-10 22:56:06 +0200

    Improve missing cowboy message

--- lib/plug/conn/adapter.ex
+++ lib/plug/conn/adapter.ex
@@ -1,7 +1,8 @@
 # In this file we check if at least one of the adapters are implemented.
 # Since right now we only support cowboy, the check is straight-forward.
 unless Code.ensure_loaded?(:cowboy_req) do
-  raise "cannot compile Plug because the :cowboy application is not available"
+  raise "cannot compile Plug because the :cowboy application is not available. " <>
+        "Please ensure it is listed as a dependency before the plug one."
 end
 
 defmodule Plug.Conn.Adapter do
