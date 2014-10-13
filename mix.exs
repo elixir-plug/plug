733f1c9f715b1c6ad089f61ea64073d202701a48
René Föhring <rf@bamaru.de>
2014-10-13 11:34:01 +0200

    Add docs badge to README

--- mix.exs
+++ mix.exs
@@ -22,6 +22,7 @@ defmodule Plug.Mixfile do
     [{:cowboy, "~> 1.0", optional: true},
      {:earmark, "~> 0.1", only: :docs},
      {:ex_doc, "~> 0.6", only: :docs},
+     {:inch_ex, only: :docs},
      {:hackney, "~> 0.13", only: :test}]
   end
 
