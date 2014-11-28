978cbae0488d67fc3b527530d6b015024f61f2c5
José Valim <jose.valim@plataformatec.com.br>
2014-11-28 11:06:02 +0100

    Move secure_compare up to Plug.Crypto

--- lib/plug/crypto/message_verifier.ex
+++ lib/plug/crypto/message_verifier.ex
@@ -8,15 +8,13 @@ defmodule Plug.Crypto.MessageVerifier do
   cannot tamper it.
   """
 
-  use Bitwise
-
   @doc """
   Decodes and verifies the encoded binary was not tampared with.
   """
   def verify(binary, secret) when is_binary(binary) and is_binary(secret) do
     case String.split(binary, "--") do
       [content, digest] when content != "" and digest != "" ->
-        if secure_compare(digest(secret, content), digest) do
+        if Plug.Crypto.secure_compare(digest(secret, content), digest) do
           {:ok, Base.decode64!(content)}
         else
           :error
@@ -36,26 +34,5 @@ defmodule Plug.Crypto.MessageVerifier do
 
   defp digest(secret, data) do
     :crypto.hmac(:sha, secret, data) |> Base.encode64
-  end
-
-  @doc """
-  Compares the two binaries in constant-time to avoid timing attacks.
-
-  See: http://codahale.com/a-lesson-in-timing-attacks/
-  """
-  def secure_compare(left, right) do
-    if byte_size(left) == byte_size(right) do
-      arithmetic_compare(left, right, 0) == 0
-    else
-      false
-    end
-  end
-
-  defp arithmetic_compare(<<x, left :: binary>>, <<y, right :: binary>>, acc) do
-    arithmetic_compare(left, right, acc ||| (x ^^^ y))
-  end
-
-  defp arithmetic_compare(<<>>, <<>>, acc) do
-    acc
   end
 end
