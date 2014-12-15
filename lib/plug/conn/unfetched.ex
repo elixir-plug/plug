9bf1fd56fab1f39db52508861566808e764514c6
Andrea Leopardi <an.leopardi@gmail.com>
2014-12-15 01:23:38 +0100

    Some refactoring and docs for Plug.Conn and Plug.Conn.Unfetched
    Plug.Conn.Unfetched: just some added docs.
    
    Plug.Conn:
    - More detailed docs overall
    - Refactored `full_path/1` in order to remove the use of a private function
    - Major refactor to `configure_session/2`
    - Replaces two calls to Erlang's :lists with Elixir's `List` module
    - Added the `port_number` type (0..65335)

--- lib/plug/conn/unfetched.ex
+++ lib/plug/conn/unfetched.ex
@@ -1,6 +1,13 @@
 defmodule Plug.Conn.Unfetched do
   @moduledoc """
   A struct used as default on unfetched fields.
+
+  The `:aspect` key of the struct specifies what field is still unfetched.
+
+  ## Examples
+
+      unfetched = %Plug.Conn.Unfetched{aspect: :cookies}
+
   """
 
   defstruct [:aspect]
