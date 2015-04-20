e14acc768e3a34e3469320abf4ce084967bf9c11
Tony Han <h.bing612@gmail.com>
2015-04-20 14:01:51 -0700

    remove exclude option and refactor other logic

--- lib/plug/ssl.ex
+++ lib/plug/ssl.ex
@@ -10,40 +10,41 @@ defmodule Plug.SSL do
     subdomains = Keyword.get(opts, :subdomains, false)
     hsts = if hsts, do: [expires: expires, subdomains: subdomains], else: false
 
-    exclude = Keyword.get(opts, :exclude)
     host = Keyword.get(opts, :host)
 
-    %{hsts: hsts,
-      exclude: exclude,
+    %{hsts_header: hsts_header(hsts),
       host: host}
   end
 
   def call(conn, config) do
-    cond do
-      config[:exclude] && config[:exclude].(conn) -> conn
-      conn.scheme == :https ->
-        register_before_send(conn, &add_hsts_headers(&1, config[:hsts]))
-      true -> redirect_to_https(conn, config[:host])
+    if conn.scheme == :https do
+      register_before_send(conn, &(put_hsts_header(&1, config[:hsts_header])))
+    else
+      redirect_to_https(conn, config[:host])
     end
   end
 
   # http://tools.ietf.org/html/draft-hodges-strict-transport-sec-02
-  defp add_hsts_headers(conn, false), do: conn
-  defp add_hsts_headers(conn, hsts) do
+  defp hsts_header(false), do: nil
+  defp hsts_header(hsts) do
     value = "max-age=#{hsts[:expires]}"
-    if hsts[:subdomains], do: value = "#{value}; includeSubDomains"
-    put_resp_header(conn, "strict-transport-security", value)
+    if hsts[:subdomains], do: "#{value}; includeSubDomains", else: value
   end
+
+  defp put_hsts_header(conn, hsts_header) when is_binary(hsts_header) do
+    put_resp_header(conn, "strict-transport-security", hsts_header)
+  end
+  defp put_hsts_header(conn, _), do: conn
 
   defp redirect_to_https(%Conn{host: host} = conn, custom_host) do
     status = if conn.method in ~w(HEAD GET), do: 301, else: 307
 
     uri = %URI{scheme: "https", host: custom_host || host,
                path: full_path(conn), query: conn.query_string}
-    headers = [{"content-type", "text/html"}, {"location", to_string(uri)}]
 
-    %{conn | resp_headers: headers}
-      |> send_resp(status, "")
-      |> halt
+    conn
+    |> put_resp_header("location", to_string(uri))
+    |> send_resp(status, "")
+    |> halt
   end
 end
