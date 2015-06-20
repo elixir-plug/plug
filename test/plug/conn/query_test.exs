a7fe50a1abdcd6d0708b0524afa6c8def2463771
Chris McCord <chris@chrismccord.com>
2015-06-20 13:13:58 +0200

    Fix empty params being encoded into query string as '&'

--- test/plug/conn/query_test.exs
+++ test/plug/conn/query_test.exs
@@ -126,4 +126,8 @@ defmodule Plug.Conn.QueryTest do
     assert encode(%{foo: URI.parse("/bar")}, encoder) ==
            "foo=%2Fbar%2Fbar"
   end
+
+  test "encode ignores empty maps" do
+    assert encode(%{filter: %{}, foo: "bar", baz: "bat"}) == "baz=bat&foo=bar"
+  end
 end
