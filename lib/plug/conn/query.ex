f554ffaf725ace0031ce645b30fdd1a4177c2b94
José Valim <jose.valim@plataformatec.com.br>
2015-06-22 15:52:29 +0200

    Speed up query encoding

--- lib/plug/conn/query.ex
+++ lib/plug/conn/query.ex
@@ -156,12 +156,12 @@ defmodule Plug.Conn.Query do
   Encodes the given dict.
   """
   def encode(dict, encoder \\ &to_string/1) do
-    encode_pair(nil, dict, encoder)
+    IO.iodata_to_binary encode_pair("", dict, encoder)
   end
 
   # covers structs
   defp encode_pair(field, %{__struct__: struct} = map, encoder) when is_atom(struct) do
-    field <> "=" <> encode_value(map, encoder)
+    [field, ?=|encode_value(map, encoder)]
   end
 
   # covers maps
@@ -176,32 +176,34 @@ defmodule Plug.Conn.Query do
 
   # covers non-keyword lists
   defp encode_pair(parent_field, list, encoder) when is_list(list) do
-    Enum.map_join list, "&", &encode_pair("#{parent_field}[]", &1, encoder)
+    prune Enum.flat_map list, fn value ->
+      [?&, encode_pair(parent_field <> "[]", value, encoder)]
+    end
   end
 
   # covers nil
   defp encode_pair(field, nil, _encoder) do
-    field <> "="
+    [field|?=]
   end
 
   # encoder fallback
   defp encode_pair(field, value, encoder) do
-    field <> "=" <> encode_value(value, encoder)
+    [field, ?=|encode_value(value, encoder)]
   end
 
   defp encode_dict(dict, parent_field, encoder) do
-    dict
-    |> Stream.reject(fn {_field, val} -> val == %{} || val == [] end)
-    |> Stream.map(fn {field, value} ->
-      field =
-        if parent_field do
-          "#{parent_field}[#{encode_key(field)}]"
-        else
-          encode_key(field)
-        end
-      encode_pair(field, value, encoder)
-    end)
-    |> Enum.join("&")
+    prune Enum.flat_map dict, fn
+      {_, value} when value in [%{}, []] ->
+        []
+      {field, value} ->
+        field =
+          if parent_field == "" do
+            encode_key(field)
+          else
+            parent_field <> "[" <> encode_key(field) <> "]"
+          end
+        [?&, encode_pair(field, value, encoder)]
+    end
   end
 
   defp encode_key(item) do
@@ -211,4 +213,7 @@ defmodule Plug.Conn.Query do
   defp encode_value(item, encoder) do
     item |> encoder.() |> URI.encode_www_form
   end
+
+  defp prune([?&|t]), do: t
+  defp prune([]), do: []
 end
