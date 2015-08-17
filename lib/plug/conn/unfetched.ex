817edf883036a2d040d2b88320482b991d3f7525
José Valim <jose.valim@plataformatec.com.br>
2015-08-17 14:48:43 +0200

    Improve error message for unfetched data

--- lib/plug/conn/unfetched.ex
+++ lib/plug/conn/unfetched.ex
@@ -12,4 +12,14 @@ defmodule Plug.Conn.Unfetched do
 
   defstruct [:aspect]
   @type t :: %__MODULE__{aspect: atom()}
+
+  @doc false
+  def get(%{aspect: aspect}, key, _value) do
+    raise ArgumentError, "cannot get key #{inspect key} from conn.#{aspect} because they were not fetched"
+  end
+
+  @doc false
+  def get_and_update(%{aspect: aspect}, key, _fun) do
+    raise ArgumentError, "cannot get_and_update key #{inspect key} from conn.#{aspect} because they were not fetched"
+  end
 end
