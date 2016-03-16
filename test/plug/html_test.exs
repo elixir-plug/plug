270e90908d0241ad3ca9dd5f1f505f61ebbc6984
Daniel Perez <daniel@claudetech.com>
2016-03-16 18:14:33 +0900

    Add tests for HTML plug.

--- test/plug/html_test.exs
+++ test/plug/html_test.exs
@@ -1,4 +1,13 @@
 defmodule Plug.HTMlTest do
   use ExUnit.Case, async: true
   doctest Plug.HTML
+
+  import Plug.HTML, only: [html_escape: 1]
+
+  test "escapes HTML" do
+    assert html_escape("<script>") == "&lt;script&gt;"
+    assert html_escape("html&company") == "html&amp;company"
+    assert html_escape("\"quoted\"") == "&quot;quoted&quot;"
+    assert html_escape("html's test") == "html&#39;s test"
+  end
 end
