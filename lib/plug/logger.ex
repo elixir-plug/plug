3aac9910f0dba906b5607f087ba3131ee97e0871
Mike Evans <michael_evans@comcast.com>
2016-03-30 16:11:22 -0400

    Ensure Elixir < 1.2.0 is supported and improve on some names

--- lib/plug/logger.ex
+++ lib/plug/logger.ex
@@ -28,12 +28,12 @@ defmodule Plug.Logger do
       [conn.method, ?\s, conn.request_path]
     end
 
-    before_time = current_time()
+    start = current_time()
 
     Conn.register_before_send(conn, fn conn ->
       Logger.log level, fn ->
-        after_time = current_time()
-        diff = (after_time - before_time) |> :erlang.convert_time_unit(:native, :micro_seconds)
+        stop = current_time()
+        diff = time_diff(start, stop)
 
         [connection_type(conn), ?\s, Integer.to_string(conn.status),
          " in ", formatted_diff(diff)]
@@ -42,7 +42,14 @@ defmodule Plug.Logger do
     end)
   end
 
-  defp current_time, do: :erlang.monotonic_time
+  # TODO: remove this once Plug supports only Elixir 1.2.
+  if function_exported?(:erlang, :monotonic_time, 0) do
+    defp current_time, do: :erlang.monotonic_time
+    defp time_diff(start, stop), do: (stop - start) |> :erlang.convert_time_unit(:native, :micro_seconds)
+  else
+    defp current_time, do: :os.timestamp()
+    defp time_diff(start, stop), do: :timer.now_diff(stop, start)
+  end
 
   defp formatted_diff(diff) when diff > 1000, do: [diff |> div(1000) |> Integer.to_string, "ms"]
   defp formatted_diff(diff), do: [diff |> Integer.to_string, "µs"]
