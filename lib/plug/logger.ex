660906b9db64dc10b7fcaee578bdc18072c88e16
Mike Evans <michael_evans@comcast.com>
2016-03-30 15:41:36 -0400

    Switch :os.timestamp to :erlang.monotonic_time for Plug.Logger

--- lib/plug/logger.ex
+++ lib/plug/logger.ex
@@ -28,18 +28,21 @@ defmodule Plug.Logger do
       [conn.method, ?\s, conn.request_path]
     end
 
-    before_time = :os.timestamp()
+    before_time = current_time()
 
     Conn.register_before_send(conn, fn conn ->
       Logger.log level, fn ->
-        after_time = :os.timestamp()
-        diff = :timer.now_diff(after_time, before_time)
+        after_time = current_time()
+        diff = (after_time - before_time) |> :erlang.convert_time_unit(:native, :micro_seconds)
+
         [connection_type(conn), ?\s, Integer.to_string(conn.status),
          " in ", formatted_diff(diff)]
       end
       conn
     end)
   end
+
+  defp current_time, do: :erlang.monotonic_time
 
   defp formatted_diff(diff) when diff > 1000, do: [diff |> div(1000) |> Integer.to_string, "ms"]
   defp formatted_diff(diff), do: [diff |> Integer.to_string, "µs"]
