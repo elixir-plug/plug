69c64954e817ca92111a69fa1804c2eec12e4701
Ria Cataquian <icataquian@gmail.com>
2016-06-19 01:11:21 +0800

    Makes use of Map instead of Dict

--- lib/plug/conn/query.ex
+++ lib/plug/conn/query.ex
@@ -25,7 +25,7 @@ defmodule Plug.Conn.Query do
       iex> decode("foo[]=bar&foo[]=baz")["foo"]
       ["bar", "baz"]
 
-  Dicts can be encoded:
+  Maps can be encoded:
 
       iex> encode(%{foo: "bar", baz: "bat"})
       "baz=bat&foo=bar"
@@ -163,10 +163,10 @@ defmodule Plug.Conn.Query do
   defp assign_list(t, value),  do: assign_parts(t, value, %{})
 
   @doc """
-  Encodes the given dict.
+  Encodes the given map.
   """
-  def encode(dict, encoder \\ &to_string/1) do
-    IO.iodata_to_binary encode_pair("", dict, encoder)
+  def encode(map, encoder \\ &to_string/1) do
+    IO.iodata_to_binary encode_pair("", map, encoder)
   end
 
   # covers structs
@@ -176,12 +176,12 @@ defmodule Plug.Conn.Query do
 
   # covers maps
   defp encode_pair(parent_field, %{} = map, encoder) do
-    encode_dict(map, parent_field, encoder)
+    encode_map(map, parent_field, encoder)
   end
 
   # covers keyword lists
   defp encode_pair(parent_field, list, encoder) when is_list(list) and is_tuple(hd(list)) do
-    encode_dict(Enum.uniq(list, &elem(&1, 0)), parent_field, encoder)
+    encode_map(Enum.uniq(list, &elem(&1, 0)), parent_field, encoder)
   end
 
   # covers non-keyword lists
@@ -201,8 +201,8 @@ defmodule Plug.Conn.Query do
     [field, ?=|encode_value(value, encoder)]
   end
 
-  defp encode_dict(dict, parent_field, encoder) do
-    prune Enum.flat_map dict, fn
+  defp encode_map(map, parent_field, encoder) do
+    prune Enum.flat_map map, fn
       {_, value} when value in [%{}, []] ->
         []
       {field, value} ->
