fc29f089f7ad58a73338cb0fce8397cdaa8eed49
Andrea Leopardi <an.leopardi@gmail.com>
2016-06-30 22:06:25 +0200

    Upgrade Plug to require Elixir ~> 1.2 (#416)

--- lib/plug/logger.ex
+++ lib/plug/logger.ex
@@ -28,27 +28,18 @@ defmodule Plug.Logger do
       [conn.method, ?\s, conn.request_path]
     end
 
-    start = current_time()
+    start = System.monotonic_time()
 
     Conn.register_before_send(conn, fn conn ->
       Logger.log level, fn ->
-        stop = current_time()
-        diff = time_diff(start, stop)
+        stop = System.monotonic_time()
+        diff = System.convert_time_unit(stop - start, :native, :micro_seconds)
 
         [connection_type(conn), ?\s, Integer.to_string(conn.status),
          " in ", formatted_diff(diff)]
       end
       conn
     end)
-  end
-
-  # TODO: remove this once Plug supports only Elixir 1.2.
-  if function_exported?(:erlang, :monotonic_time, 0) do
-    defp current_time, do: :erlang.monotonic_time
-    defp time_diff(start, stop), do: (stop - start) |> :erlang.convert_time_unit(:native, :micro_seconds)
-  else
-    defp current_time, do: :os.timestamp()
-    defp time_diff(start, stop), do: :timer.now_diff(stop, start)
   end
 
   defp formatted_diff(diff) when diff > 1000, do: [diff |> div(1000) |> Integer.to_string, "ms"]
