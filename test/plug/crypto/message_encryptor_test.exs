5cb2f6cfdaed880615d2c083e2b1e22c074f65a9
Andrew Bennett <potatosaladx@gmail.com>
2016-07-07 02:48:15 -0600

    Replace AES-CBC with AES-GCM and HMAC-SHA-1 with HMAC-SHA-256. (#420)
    * Replace AES-CBC with AES-GCM and HMAC-SHA-1 with HMAC-SHA-256.
    
    * Remove reporting on failure for AES-CBC mode from tests.
    
    * Add TODO comments about with/else statements in MessageEncryptor and MessageVerifier.
    
    * Add TODO about switching to verify/3 after backwards compatibility period.

--- test/plug/crypto/message_encryptor_test.exs
+++ test/plug/crypto/message_encryptor_test.exs
@@ -9,13 +9,32 @@ defmodule Plug.Crypto.MessageEncryptorTest do
 
   test "it encrypts/decrypts a message" do
     data = <<0, "hełłoworld", 0>>
-    encrypted = ME.encrypt_and_sign(<<0, "hełłoworld", 0>>, @right, @right)
+    encrypted = ME.encrypt(<<0, "hełłoworld", 0>>, @right, @right)
+
+    decrypted = ME.decrypt(encrypted, @wrong, @wrong)
+    assert decrypted == :error
+
+    decrypted = ME.decrypt(encrypted, @right, @wrong)
+    assert decrypted == :error
+
+    decrypted = ME.decrypt(encrypted, @wrong, @right)
+    assert decrypted == :error
+
+    decrypted = ME.decrypt(encrypted, @right, @right)
+    assert decrypted == {:ok, data}
+
+    # Legacy support for AES256-CBC with HMAC SHA-1
+    encrypted = ME.encrypt_and_sign(<<0, "hełłoworld", 0>>, @right, @right, :aes_cbc256)
+
+    decrypted = ME.verify_and_decrypt(encrypted, @wrong, @wrong)
+    assert decrypted == :error
 
     decrypted = ME.verify_and_decrypt(encrypted, @right, @wrong)
     assert decrypted == :error
 
-    decrypted = ME.verify_and_decrypt(encrypted, @wrong, @right)
-    assert decrypted == :error
+    # This can fail depending on the initialization vector
+    # decrypted = ME.verify_and_decrypt(encrypted, @wrong, @right)
+    # assert decrypted == :error
 
     decrypted = ME.verify_and_decrypt(encrypted, @right, @right)
     assert decrypted == {:ok, data}
@@ -23,7 +42,36 @@ defmodule Plug.Crypto.MessageEncryptorTest do
 
   test "it uses only the first 32 bytes to encrypt/decrypt" do
     data = <<0, "helloworld", 0>>
-    encrypted = ME.encrypt_and_sign(<<0, "helloworld", 0>>, @large, @large)
+    encrypted = ME.encrypt(<<0, "helloworld", 0>>, @large, @large)
+
+    decrypted = ME.decrypt(encrypted, @large, @large)
+    assert decrypted == {:ok, data}
+
+    decrypted = ME.decrypt(encrypted, @right, @large)
+    assert decrypted == {:ok, data}
+
+    decrypted = ME.decrypt(encrypted, @large, @right)
+    assert decrypted == :error
+
+    decrypted = ME.decrypt(encrypted, @right, @right)
+    assert decrypted == :error
+
+    encrypted = ME.encrypt(<<0, "helloworld", 0>>, @right, @large)
+
+    decrypted = ME.decrypt(encrypted, @large, @large)
+    assert decrypted == {:ok, data}
+
+    decrypted = ME.decrypt(encrypted, @right, @large)
+    assert decrypted == {:ok, data}
+
+    decrypted = ME.decrypt(encrypted, @large, @right)
+    assert decrypted == :error
+
+    decrypted = ME.decrypt(encrypted, @right, @right)
+    assert decrypted == :error
+
+    # Legacy support for AES256-CBC with HMAC SHA-1
+    encrypted = ME.encrypt_and_sign(<<0, "helloworld", 0>>, @large, @large, :aes_cbc256)
 
     decrypted = ME.verify_and_decrypt(encrypted, @large, @large)
     assert decrypted == {:ok, data}
@@ -37,7 +85,7 @@ defmodule Plug.Crypto.MessageEncryptorTest do
     decrypted = ME.verify_and_decrypt(encrypted, @right, @right)
     assert decrypted == :error
 
-    encrypted = ME.encrypt_and_sign(<<0, "helloworld", 0>>, @right, @large)
+    encrypted = ME.encrypt_and_sign(<<0, "helloworld", 0>>, @right, @large, :aes_cbc256)
 
     decrypted = ME.verify_and_decrypt(encrypted, @large, @large)
     assert decrypted == {:ok, data}
