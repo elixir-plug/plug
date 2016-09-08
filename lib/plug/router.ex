b18742479ccff6defe7c91a925c2d37b9ea15013
Jay Hayes <jay@bignerdranch.com>
2016-09-08 12:10:12 -0500

    Illustrate guarded routes with additional URI segment (#449)
    The original wording tripped me up a bit as the URI segment "bar"
    suddenly became dynamic in the subsequent example. This builds on the
    previous examples by adding a new dynamic segment for subsequent
    examples.

--- lib/plug/router.ex
+++ lib/plug/router.ex
@@ -138,14 +138,14 @@ defmodule Plug.Router do
   This opens up a few possibilities. First, guards can be given
   to `match`:
 
-      match "/foo/:bar" when size(bar) <= 3, via: :get do
+      match "/foo/bar/:baz" when size(baz) <= 3, via: :get do
         send_resp(conn, 200, "hello world")
       end
 
   Second, a list of split paths (which is the compiled result) is
   also allowed:
 
-      match ["foo", bar], via: :get do
+      match ["foo", "bar", baz], via: :get do
         send_resp(conn, 200, "hello world")
       end
 
