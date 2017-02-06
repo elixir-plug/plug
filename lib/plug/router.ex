c8fe83b74d01455beb8c7784db93e0830aff184f
Matt Gowie <gowie.matt@gmail.com>
2017-02-06 03:46:05 -0500

    Adds 'Parameter Parsing' section to Router docs (#510)

--- lib/plug/router.ex
+++ lib/plug/router.ex
@@ -85,6 +85,38 @@ defmodule Plug.Router do
   Check `match/3` for more information on how route compilation
   works and a list of supported options.
 
+  ## Parameter Parsing
+
+  Handling request data can be done through the
+  [`Plug.Parsers`](https://hexdocs.pm/plug/Plug.Parsers.html#content) module. It
+  provides support for parsing url encoded, form-data, and json data as well as
+  providing a behaviour that all other Parsers should adopt.
+
+  Here is an example of how it can be used to parse a JSON POST Body:
+
+      defmodule AppRouter do
+        use Plug.Router
+
+        plug :match
+        plug Plug.Parsers, parsers: [:json],
+                           pass:  ["application/json"],
+                           json_decoder: Poison
+        plug :dispatch
+
+        post "/hello" do
+          IO.puts conn.body_params # Prints JSON POST body
+          send_resp(conn, 200, "Success!")
+        end
+      end
+
+  It is important that `Plug.Parsers` is ordered prior to the `:dispatch` plug
+  otherwise the matched function will not receive the parsed `Plug.Conn`.
+
+  We could also plug `Plug.Parsers` after :match (but still before `:dispatch`),
+  this means `Plug.Parsers` will only run if there is matching route. It is
+  useful to perform actions such as authentication, which should only execute
+  after an appropriate route is found.
+
   ## Error handling
 
   In case something goes wrong in a request, the router by default
