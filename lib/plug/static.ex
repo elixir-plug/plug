5e8ce2bb3ba58855171792c826dda21c79cdcefb
Nicolas Goy <kuon@goyman.com>
2017-03-19 16:00:18 +0100

    Add support for custom MIME types (#530)

--- lib/plug/static.ex
+++ lib/plug/static.ex
@@ -77,6 +77,10 @@ defmodule Plug.Static do
 
     * `:headers` - other headers to be set when serving static assets.
 
+    * `:content_types` - custom MIME type mapping. As a map with filename as key
+      and content type as value. For example:
+      `content_types: %{"apple-app-site-association" => "application/json"}`.
+
   ## Examples
 
   This plug can be mounted in a `Plug.Builder` pipeline as follows:
@@ -134,6 +138,7 @@ defmodule Plug.Static do
       et_cache: Keyword.get(opts, :cache_control_for_etags, "public"),
       et_generation: Keyword.get(opts, :etag_generation, nil),
       headers: Keyword.get(opts, :headers, %{}),
+      content_types: Keyword.get(opts, :content_types, %{}),
       from: from,
       at: opts |> Keyword.fetch!(:at) |> Plug.Router.Utils.split()
     }
@@ -179,10 +184,11 @@ defmodule Plug.Static do
 
   defp serve_static({:ok, conn, file_info, path}, segments, options) do
     %{qs_cache: qs_cache, et_cache: et_cache, et_generation: et_generation,
-      gzip?: gzip?, brotli?: brotli?, headers: headers} = options
+      gzip?: gzip?, brotli?: brotli?, headers: headers, content_types: types} = options
     case put_cache_header(conn, qs_cache, et_cache, et_generation, file_info, path) do
       {:stale, conn} ->
-        content_type = segments |> List.last |> MIME.from_path
+        filename = List.last(segments)
+        content_type = Map.get(types, filename) || MIME.from_path(filename)
 
         conn
         |> maybe_add_vary(gzip?, brotli?)
