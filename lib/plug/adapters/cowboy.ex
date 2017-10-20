fc5dc2a99b6340cb1269437a510ea432adcf3278
Jaremy Creechley <creechley@gmail.com>
2017-10-20 00:37:11 -0600

    Update cowboy.ex

--- lib/plug/adapters/cowboy.ex
+++ lib/plug/adapters/cowboy.ex
@@ -222,7 +222,7 @@ defmodule Plug.Adapters.Cowboy do
   defp add_on_response(false, fun, protocol_options) when is_function(fun), do: [onresponse: fun] ++ protocol_options
   defp add_on_response(false, {mod, fun}, protocol_options) when is_atom(mod) and is_atom(fun) do 
     [onresponse: fn status, headers, body, request -> 
-        apply(mod, func, [status, headers, body, request]) 
+        apply(mod, fun, [status, headers, body, request]) 
      end ] ++ protocol_options
   end
   defp add_on_response(true, nil, protocol_options), do: [onresponse: &onresponse/4] ++ protocol_options
