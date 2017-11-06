elixir-plug/plug

commit 3575726336f919ace3da619aad06db3c80242d26

Fix start function for Cowboy 2 adapter (#618)
Previously, the cowboy start_clear/start_tls functions were being used directly.
This meant that they were being started under the supervision tree for ranch and
not the supervision tree for the actual application. This approach use
`ranch_listener_sup` as the entry point instead. This matches what happens in
the Cowboy adapter.

lib/plug/adapters/cowboy2.ex
@@ -146,17 +146,27 @@ defmodule Plug.Adapters.Cowboy2 do
       end
 
     cowboy_args = args(scheme, plug, plug_opts, cowboy_opts)
-    [ref | _] = cowboy_args
+    [ref, transport_opts, proto_opts] = cowboy_args
 
-    cowboy_function =
+    {ranch_module, cowboy_protocol} =
       case scheme do
-        :http -> :start_clear
-        :https -> :start_tls
+        :http -> {:ranch_tcp, :cowboy_clear}
+        :https -> {:ranch_ssl, :cowboy_tls}
       end
+
+    num_acceptors = Keyword.get(transport_opts, :num_acceptors, 100)
 
     %{
       id: {:ranch_listener_sup, ref},
-      start: {:cowboy, cowboy_function, cowboy_args},
+      start:
+        {:ranch_listener_sup, :start_link, [
+          ref,
+          num_acceptors,
+          ranch_module,
+          transport_opts,
+          cowboy_protocol,
+          proto_opts
+        ]},
       restart: :permanent,
       shutdown: :infinity,
       type: :supervisor,

test/plug/adapters/cowboy2_test.exs
@@ -21,7 +21,7 @@ defmodule Plug.Adapters.Cowboy2Test do
                modules: [:ranch_listener_sup],
                restart: :permanent,
                shutdown: :infinity,
-               start: {:cowboy, :start_clear, _},
+               start: {:ranch_listener_sup, :start_link, _},
                type: :supervisor
              } = Supervisor.child_spec(spec, [])
     end
@@ -113,7 +113,7 @@ defmodule Plug.Adapters.Cowboy2Test do
     assert %{
              id: {:ranch_listener_sup, Plug.Adapters.Cowboy2Test.HTTP},
              modules: [:ranch_listener_sup],
-             start: {:cowboy, :start_clear, _},
+             start: {:ranch_listener_sup, :start_link, _},
              restart: :permanent,
              shutdown: :infinity,
              type: :supervisor

commit 8cec4d7b3d651ef119867e9b5c88c858d68be44a

Add the ALPN options for HTTP/2 when using SSL
This was broken in 3575726336f919ace3da619aad06db3c80242d26 when
switching from using `cowboy.start_tls` to ranch_listener_sup. The
required options from `cowboy.start_tls` have been added to the
transport options.

https://github.com/ninenines/cowboy/blob/b2bd9ccfb582213909129b6a54514b968856168a/src/cowboy.erl#L49

lib/plug/adapters/cowboy2.ex
@@ -148,10 +148,18 @@ defmodule Plug.Adapters.Cowboy2 do
     cowboy_args = args(scheme, plug, plug_opts, cowboy_opts)
     [ref, transport_opts, proto_opts] = cowboy_args
 
-    {ranch_module, cowboy_protocol} =
+    {ranch_module, cowboy_protocol, transport_opts} =
       case scheme do
-        :http -> {:ranch_tcp, :cowboy_clear}
-        :https -> {:ranch_ssl, :cowboy_tls}
+        :http ->
+          {:ranch_tcp, :cowboy_clear, transport_opts}
+
+        :https ->
+          transport_opts =
+            transport_opts
+            |> Keyword.put_new(:next_protocols_advertised, ["h2", "http/1.1"])
+            |> Keyword.put_new(:alpn_preferred_protocols, ["h2", "http/1.1"])
+
+          {:ranch_ssl, :cowboy_tls, transport_opts}
       end
 
     num_acceptors = Keyword.get(transport_opts, :num_acceptors, 100)

test/plug/adapters/cowboy2_test.exs
@@ -25,6 +25,31 @@ defmodule Plug.Adapters.Cowboy2Test do
                type: :supervisor
              } = Supervisor.child_spec(spec, [])
     end
+
+    test "the h2 alpn settings are added when using https" do
+      options = [
+        port: 4040,
+        password: "cowboy",
+        keyfile: Path.expand("../../fixtures/ssl/key.pem", __DIR__),
+        certfile: Path.expand("../../fixtures/ssl/cert.pem", __DIR__)
+      ]
+
+      spec = {Plug.Adapters.Cowboy2, [scheme: :https, plug: __MODULE__, options: options]}
+
+      %{start: {:ranch_listener_sup, :start_link, opts}} = Supervisor.child_spec(spec, [])
+
+      assert [
+               Plug.Adapters.Cowboy2Test.HTTPS,
+               100,
+               :ranch_ssl,
+               transport_opts,
+               :cowboy_tls,
+               _proto_opts
+             ] = opts
+
+      assert Keyword.get(transport_opts, :alpn_preferred_protocols) == ["h2", "http/1.1"]
+      assert Keyword.get(transport_opts, :next_protocols_advertised) == ["h2", "http/1.1"]
+    end
   end
 
   test "builds args for cowboy dispatch" do