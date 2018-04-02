79158d1da1cc842e63a7b9b7825418d59e064977
José Valim <jose.valim@gmail.com>
2018-04-02 12:02:20 +0200

    Update conn.ex

--- lib/plug/conn.ex
+++ lib/plug/conn.ex
@@ -802,7 +802,8 @@ defmodule Plug.Conn do
   @doc """
   Fetches query parameters from the query string.
 
-  Params are decoded as "x-www-form-urlencoded".
+  Params are decoded as "x-www-form-urlencoded" in which key/value pairs
+  are separated by `&` and keys are separated from values by `=`.
 
   This function does not fetch parameters from the body. To fetch
   parameters from the body, use the `Plug.Parsers` plug.
