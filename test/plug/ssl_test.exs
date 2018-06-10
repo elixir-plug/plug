2541c66764db932eafca94d08592acac7497d2e6
José Valim <jose.valim@plataformatec.com.br>
2018-06-11 00:25:52 +0200

    Improve Plug.SSL coverage

--- test/plug/ssl_test.exs
+++ test/plug/ssl_test.exs
@@ -2,142 +2,159 @@ defmodule Plug.SSLTest do
   use ExUnit.Case, async: true
   use Plug.Test
 
-  import ExUnit.CaptureLog
+  describe "configure" do
+    import Plug.SSL, only: [configure: 1]
+
+    test "sets secure_renegociate and renew_sessions to true by default" do
+      assert {:ok, opts} = configure(key: "abcdef", cert: "ghijkl")
+      assert opts[:reuse_sessions] == true
+      assert opts[:secure_renegotiate] == true
+
+      assert {:ok, opts} = configure(key: "abcdef", cert: "ghijkl", reuse_sessions: false)
+      assert opts[:reuse_sessions] == false
+    end
+  end
 
   defp call(conn, opts \\ []) do
     opts = Keyword.put_new(opts, :log, false)
     Plug.SSL.call(conn, Plug.SSL.init(opts))
   end
 
-  test "HSTS headers by default" do
-    conn = call(conn(:get, "https://example.com/"))
-    assert get_resp_header(conn, "strict-transport-security") == ["max-age=31536000"]
-    refute conn.halted
-  end
+  describe "HSTS" do
+    test "includes headers by default" do
+      conn = call(conn(:get, "https://example.com/"))
+      assert get_resp_header(conn, "strict-transport-security") == ["max-age=31536000"]
+      refute conn.halted
+    end
 
-  test "HSTS headers excludes localhost" do
-    conn = call(conn(:get, "https://localhost/"))
-    assert get_resp_header(conn, "strict-transport-security") == []
-    refute conn.halted
-  end
+    test "excludes localhost" do
+      conn = call(conn(:get, "https://localhost/"))
+      assert get_resp_header(conn, "strict-transport-security") == []
+      refute conn.halted
+    end
 
-  test "HSTS is true" do
-    conn = call(conn(:get, "https://example.com/"), hsts: true)
-    assert get_resp_header(conn, "strict-transport-security") == ["max-age=31536000"]
-    refute conn.halted
-  end
+    test "when true" do
+      conn = call(conn(:get, "https://example.com/"), hsts: true)
+      assert get_resp_header(conn, "strict-transport-security") == ["max-age=31536000"]
+      refute conn.halted
+    end
 
-  test "HSTS is false" do
-    conn = call(conn(:get, "https://example.com/"), hsts: false)
-    assert get_resp_header(conn, "strict-transport-security") == []
-    refute conn.halted
-  end
+    test "when false" do
+      conn = call(conn(:get, "https://example.com/"), hsts: false)
+      assert get_resp_header(conn, "strict-transport-security") == []
+      refute conn.halted
+    end
 
-  test "HSTS custom expires" do
-    conn = call(conn(:get, "https://example.com/"), expires: 3600)
-    assert get_resp_header(conn, "strict-transport-security") == ["max-age=3600"]
-    refute conn.halted
-  end
+    test "with custom expires" do
+      conn = call(conn(:get, "https://example.com/"), expires: 3600)
+      assert get_resp_header(conn, "strict-transport-security") == ["max-age=3600"]
+      refute conn.halted
+    end
 
-  test "HSTS include subdomains" do
-    conn = call(conn(:get, "https://example.com/"), subdomains: true)
+    test "includes subdomains" do
+      conn = call(conn(:get, "https://example.com/"), subdomains: true)
 
-    assert get_resp_header(conn, "strict-transport-security") ==
-             ["max-age=31536000; includeSubDomains"]
+      assert get_resp_header(conn, "strict-transport-security") ==
+               ["max-age=31536000; includeSubDomains"]
 
-    refute conn.halted
-  end
+      refute conn.halted
+    end
 
-  test "HSTS include preload" do
-    conn = call(conn(:get, "https://example.com/"), preload: true)
-    assert get_resp_header(conn, "strict-transport-security") == ["max-age=31536000; preload"]
-    refute conn.halted
-  end
+    test "includes preload" do
+      conn = call(conn(:get, "https://example.com/"), preload: true)
+      assert get_resp_header(conn, "strict-transport-security") == ["max-age=31536000; preload"]
+      refute conn.halted
+    end
 
-  test "HSTS with multiple flags" do
-    conn = call(conn(:get, "https://example.com/"), preload: true, subdomains: true)
+    test "with multiple flags" do
+      conn = call(conn(:get, "https://example.com/"), preload: true, subdomains: true)
 
-    assert get_resp_header(conn, "strict-transport-security") ==
-             ["max-age=31536000; preload; includeSubDomains"]
+      assert get_resp_header(conn, "strict-transport-security") ==
+               ["max-age=31536000; preload; includeSubDomains"]
 
-    refute conn.halted
-  end
-
-  test "rewrites conn http to https based on x-forwarded-proto" do
-    conn =
-      conn(:get, "http://example.com/")
-      |> put_req_header("x-forwarded-proto", "https")
-      |> call(rewrite_on: [:x_forwarded_proto])
-
-    assert get_resp_header(conn, "strict-transport-security") == ["max-age=31536000"]
-    refute conn.halted
-  end
-
-  test "redirects to host when insecure" do
-    conn = call(conn(:get, "http://example.com/"))
-    assert get_resp_header(conn, "location") == ["https://example.com/"]
-    assert conn.halted
-
-    conn = call(conn(:get, "http://example.com/foo?bar=baz"))
-    assert get_resp_header(conn, "location") == ["https://example.com/foo?bar=baz"]
-    assert conn.halted
-  end
-
-  test "redirects to host when secure" do
-    conn =
-      conn(:get, "https://example.com/")
-      |> put_req_header("x-forwarded-proto", "http")
-      |> call(rewrite_on: [:x_forwarded_proto])
-
-    assert get_resp_header(conn, "location") == ["https://example.com/"]
-    assert conn.halted
-
-    conn =
-      conn(:get, "https://example.com/foo?bar=baz")
-      |> put_req_header("x-forwarded-proto", "http")
-      |> call(rewrite_on: [:x_forwarded_proto])
-
-    assert get_resp_header(conn, "location") == ["https://example.com/foo?bar=baz"]
-    assert conn.halted
-  end
-
-  test "redirects to custom host on get" do
-    conn = call(conn(:get, "http://example.com/"), host: "ssl.example.com:443")
-    assert get_resp_header(conn, "location") == ["https://ssl.example.com:443/"]
-    assert conn.status == 301
-    assert conn.halted
-  end
-
-  test "redirects to tuple host on get" do
-    System.put_env("PLUG_SSL_HOST", "ssl.example.com:443")
-    conn = call(conn(:get, "http://example.com/"), host: {System, :get_env, ["PLUG_SSL_HOST"]})
-    assert get_resp_header(conn, "location") == ["https://ssl.example.com:443/"]
-    assert conn.status == 301
-    assert conn.halted
-  end
-
-  test "redirects to host on head" do
-    conn = call(conn(:head, "http://example.com/"))
-    assert conn.status == 301
-    assert conn.halted
-  end
-
-  test "redirects to custom host with other verbs" do
-    for method <- ~w(options post put delete patch)a do
-      conn = call(conn(method, "http://example.com/"))
-      assert conn.status == 307
-      assert conn.halted
+      refute conn.halted
     end
   end
 
-  test "logs on redirect" do
-    message =
-      capture_log(fn ->
-        conn = call(conn(:get, "http://example.com/"), log: :info)
-        assert get_resp_header(conn, "location") == ["https://example.com/"]
-        assert conn.halted
-      end)
+  describe ":rewrite_on" do
+    test "rewrites conn http to https based on x-forwarded-proto" do
+      conn =
+        conn(:get, "http://example.com/")
+        |> put_req_header("x-forwarded-proto", "https")
+        |> call(rewrite_on: [:x_forwarded_proto])
 
-    assert message =~ ~r"Plug.SSL is redirecting GET / to https://example.com with status 301"u
+      assert get_resp_header(conn, "strict-transport-security") == ["max-age=31536000"]
+      refute conn.halted
+    end
+  end
+
+  describe "redirects" do
+    test "to host when insecure" do
+      conn = call(conn(:get, "http://example.com/"))
+      assert get_resp_header(conn, "location") == ["https://example.com/"]
+      assert conn.halted
+
+      conn = call(conn(:get, "http://example.com/foo?bar=baz"))
+      assert get_resp_header(conn, "location") == ["https://example.com/foo?bar=baz"]
+      assert conn.halted
+    end
+
+    test "to host when secure" do
+      conn =
+        conn(:get, "https://example.com/")
+        |> put_req_header("x-forwarded-proto", "http")
+        |> call(rewrite_on: [:x_forwarded_proto])
+
+      assert get_resp_header(conn, "location") == ["https://example.com/"]
+      assert conn.halted
+
+      conn =
+        conn(:get, "https://example.com/foo?bar=baz")
+        |> put_req_header("x-forwarded-proto", "http")
+        |> call(rewrite_on: [:x_forwarded_proto])
+
+      assert get_resp_header(conn, "location") == ["https://example.com/foo?bar=baz"]
+      assert conn.halted
+    end
+
+    test "to custom host on get" do
+      conn = call(conn(:get, "http://example.com/"), host: "ssl.example.com:443")
+      assert get_resp_header(conn, "location") == ["https://ssl.example.com:443/"]
+      assert conn.status == 301
+      assert conn.halted
+    end
+
+    test "to tuple host on get" do
+      System.put_env("PLUG_SSL_HOST", "ssl.example.com:443")
+      conn = call(conn(:get, "http://example.com/"), host: {System, :get_env, ["PLUG_SSL_HOST"]})
+      assert get_resp_header(conn, "location") == ["https://ssl.example.com:443/"]
+      assert conn.status == 301
+      assert conn.halted
+    end
+
+    test "to host on head" do
+      conn = call(conn(:head, "http://example.com/"))
+      assert conn.status == 301
+      assert conn.halted
+    end
+
+    test "to custom host with other verbs" do
+      for method <- ~w(options post put delete patch)a do
+        conn = call(conn(method, "http://example.com/"))
+        assert conn.status == 307
+        assert conn.halted
+      end
+    end
+
+    test "logs on redirect" do
+      message =
+        ExUnit.CaptureLog.capture_log(fn ->
+          conn = call(conn(:get, "http://example.com/"), log: :info)
+          assert get_resp_header(conn, "location") == ["https://example.com/"]
+          assert conn.halted
+        end)
+
+      assert message =~ ~r"Plug.SSL is redirecting GET / to https://example.com with status 301"u
+    end
   end
 end
