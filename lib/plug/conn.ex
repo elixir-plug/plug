fce5aebb5f177f6d1cdc8f9ae99ba98b6ccaf4ae
Eric Meadows-Jönsson <eric.meadows.jonsson@gmail.com>
2018-10-13 16:33:21 +0200

    Documenting HTTP/1.1 response streaming (#780)
    Closes #492.

--- lib/plug/conn.ex
+++ lib/plug/conn.ex
@@ -465,6 +465,13 @@ defmodule Plug.Conn do
 
   It expects a connection that has not been `:sent` yet and sets its
   state to `:chunked` afterwards. Otherwise, raises `Plug.Conn.AlreadySentError`.
+  After `send_chunked/2` is called, chunks can be sent to the client via
+  the `chunk/2` function.
+
+  HTTP/2 does not support chunking and will instead stream the response without a
+  transfer encoding. When using HTTP/1.1, the Cowboy adapter will stream the response
+  instead of emitting chunks if the `content-length` header has been set before calling
+  `send_chunked/2`.
   """
   @spec send_chunked(t, status) :: t | no_return
   def send_chunked(%Conn{state: state}, status)
