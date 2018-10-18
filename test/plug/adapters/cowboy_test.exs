2fd3a0d42a2e705f4df23b52886e05d7542cdd2c
Gary Rennie <gazler@gmail.com>
2018-10-18 21:41:06 +0100

    Replace all Plug.Adapters.Cowboy functions with warings
    Like the Cowboy2 adapter, the Cowboy adapter is no longer intended for use and
    required the `plug_cowboy` library. Unlike the Cowboy2 adapter, continued use
    will not display a deprecation warning, this is to provide better
    maintainability with existing libraries that depend on plug.
    
    The translator and its test have been removed as they are no longer relevant for
    plug directly, and should be added by the adapter.

--- test/plug/adapters/cowboy_test.exs
+++ test/plug/adapters/cowboy_test.exs
@@ -1,230 +1,84 @@
 defmodule Plug.Adapters.CowboyTest do
-  use ExUnit.Case, async: true
+  use ExUnit.Case
 
-  import Plug.Adapters.Cowboy
+  @raise_message "plug_cowboy dependency missing"
+  @missing_warning "Please add `{:plug_cowboy, \"~> 1.0\"}`"
+  @plug_cowboy_path Path.expand("../../fixtures/plug_cowboy.exs", __DIR__)
 
-  @moduletag :cowboy1
-
-  def init([]) do
-    [foo: :bar]
+  setup do
+    Code.require_file(@plug_cowboy_path)
+    :ok
   end
 
-  @dispatch [
-    {:_, [],
-     [
-       {:_, [], Plug.Adapters.Cowboy.Handler, {Plug.Adapters.CowboyTest, [foo: :bar]}}
-     ]}
-  ]
+  import ExUnit.CaptureIO
 
-  if function_exported?(Supervisor, :child_spec, 2) do
-    test "supports Elixir v1.5 child specs" do
-      spec = {Plug.Adapters.Cowboy, [scheme: :http, plug: __MODULE__, options: [port: 4040]]}
+  describe "http/3" do
+    test "raises and warns if the plug_cowboy is missing" do
+      test_raise(fn -> Plug.Adapters.Cowboy.http(__MODULE__, [], port: 8003) end)
+    end
 
-      assert %{
-               id: {:ranch_listener_sup, Plug.Adapters.CowboyTest.HTTP},
-               modules: [:ranch_listener_sup],
-               restart: :permanent,
-               shutdown: :infinity,
-               start: {:ranch_listener_sup, :start_link, _},
-               type: :supervisor
-             } = Supervisor.child_spec(spec, [])
+    test "proxies if Plug.Cowboy is defined" do
+      assert {:ok, :http} == Plug.Adapters.Cowboy.http(__MODULE__, [], port: 8003)
     end
   end
 
-  test "builds args for cowboy dispatch" do
-    assert [
-             Plug.Adapters.CowboyTest.HTTP,
-             100,
-             [port: 4000, max_connections: 16_384],
-             [env: [dispatch: @dispatch], onresponse: _]
-           ] = args(:http, __MODULE__, [], [])
-  end
-
-  test "builds args with custom options" do
-    assert [
-             Plug.Adapters.CowboyTest.HTTP,
-             25,
-             [max_connections: 16_384, port: 3000, other: true],
-             [env: [dispatch: @dispatch], onresponse: _]
-           ] = args(:http, __MODULE__, [], port: 3000, acceptors: 25, other: true)
-  end
-
-  test "builds args with non 2-element tuple options" do
-    assert [
-             Plug.Adapters.CowboyTest.HTTP,
-             25,
-             [:inet6, {:raw, 1, 2, 3}, max_connections: 16_384, port: 3000, other: true],
-             [env: [dispatch: @dispatch], onresponse: _]
-           ] =
-             args(:http, __MODULE__, [], [
-               :inet6,
-               {:raw, 1, 2, 3},
-               port: 3000,
-               acceptors: 25,
-               other: true
-             ])
-  end
-
-  test "builds args with protocol option" do
-    assert [
-             Plug.Adapters.CowboyTest.HTTP,
-             25,
-             [max_connections: 16_384, port: 3000],
-             [env: [dispatch: @dispatch], onresponse: _, compress: true, timeout: 30_000]
-           ] =
-             args(
-               :http,
-               __MODULE__,
-               [],
-               port: 3000,
-               acceptors: 25,
-               compress: true,
-               timeout: 30_000
-             )
-
-    assert [
-             Plug.Adapters.CowboyTest.HTTP,
-             25,
-             [max_connections: 16_384, port: 3000],
-             [env: [dispatch: @dispatch], onresponse: _, timeout: 30_000]
-           ] =
-             args(
-               :http,
-               __MODULE__,
-               [],
-               port: 3000,
-               acceptors: 25,
-               protocol_options: [timeout: 30_000]
-             )
-  end
-
-  test "builds args with single-atom protocol option" do
-    assert [
-             Plug.Adapters.CowboyTest.HTTP,
-             25,
-             [:inet6, max_connections: 16_384, port: 3000],
-             [env: [dispatch: @dispatch], onresponse: _]
-           ] = args(:http, __MODULE__, [], [:inet6, port: 3000, acceptors: 25])
-  end
-
-  test "builds child specs" do
-    assert {id, start, :permanent, :infinity, :supervisor, [:ranch_listener_sup]} =
-             child_spec(:http, __MODULE__, [], [])
-
-    assert id == {:ranch_listener_sup, Plug.Adapters.CowboyTest.HTTP}
-    assert {:ranch_listener_sup, :start_link, _} = start
-  end
-
-  describe "onresponse handling" do
-    test "includes the default onresponse handler" do
-      assert [
-               Plug.Adapters.CowboyTest.HTTP,
-               _,
-               _,
-               [env: [dispatch: @dispatch], onresponse: on_response]
-             ] = args(:http, __MODULE__, [], [])
-
-      assert is_function(on_response)
+  describe "https/3" do
+    test "raises and warns if the plug_cowboy is missing" do
+      test_raise(fn -> Plug.Adapters.Cowboy.https(__MODULE__, [], port: 8003) end)
     end
 
-    test "elides the default onresponse handler if log_error_on_incomplete_requests is set to false" do
-      assert [Plug.Adapters.CowboyTest.HTTP, _, _, [env: [dispatch: @dispatch]]] =
-               args(:http, __MODULE__, [], log_error_on_incomplete_requests: false)
+    test "proxies if Plug.Cowboy is defined" do
+      assert {:ok, :https} == Plug.Adapters.Cowboy.https(__MODULE__, [], port: 8003)
+    end
+  end
+
+  describe "shutdown/1" do
+    test "raises and warns if the plug_cowboy is missing" do
+      test_raise(fn -> Plug.Adapters.Cowboy.shutdown(:ref) end)
     end
 
-    test "elides the default onresponse handler if log_error_on_incomplete_requests is set to false and includes the user-provided onresponse handler" do
-      my_onresponse = fn _, _, _, req -> req end
+    test "proxies if Plug.Cowboy is defined" do
+      assert {:ok, :shutdown} == Plug.Adapters.Cowboy.shutdown(:ref)
+    end
+  end
 
-      assert [
-               Plug.Adapters.CowboyTest.HTTP,
-               _,
-               _,
-               [env: [dispatch: @dispatch], onresponse: on_response]
-             ] =
-               args(
-                 :http,
-                 __MODULE__,
-                 [],
-                 log_error_on_incomplete_requests: false,
-                 protocol_options: [onresponse: my_onresponse]
-               )
-
-      assert is_function(on_response)
-      assert on_response == my_onresponse
+  describe "child_spec/4" do
+    test "raises and warns if the plug_cowboy is missing" do
+      test_raise(fn -> Plug.Adapters.Cowboy.child_spec(:http, __MODULE__, [], []) end)
     end
 
-    test "elides the default onresponse handler if log_error_on_incomplete_requests is set to false and handles user-provided onresponse tuple " do
-      my_onresponse = {Plug.Adapters.CowboyTest, :my_onresponse_handler}
+    test "proxies if Plug.Cowboy is defined" do
+      assert {:ok, :child_spec} == Plug.Adapters.Cowboy.child_spec([])
+      {:ok, :child_spec} = Plug.Adapters.Cowboy.child_spec(:http, __MODULE__, [], [])
+    end
+  end
 
-      assert [
-               Plug.Adapters.CowboyTest.HTTP,
-               _,
-               _,
-               [env: [dispatch: @dispatch], onresponse: default_response]
-             ] = args(:http, __MODULE__, [], [])
-
-      assert [
-               Plug.Adapters.CowboyTest.HTTP,
-               _,
-               _,
-               [env: [dispatch: @dispatch], onresponse: on_response]
-             ] =
-               args(
-                 :http,
-                 __MODULE__,
-                 [],
-                 log_error_on_incomplete_requests: false,
-                 protocol_options: [onresponse: my_onresponse]
-               )
-
-      assert is_function(on_response)
-      assert on_response != default_response
+  describe "child_spec/1" do
+    test "raises and warns if the plug_cowboy is missing" do
+      test_raise(fn -> Plug.Adapters.Cowboy.child_spec([]) end)
     end
 
-    test "includes the default onresponse handler and the user-provided onresponse handler" do
-      # Grab a ref to the default onresponse handler
-      assert [
-               Plug.Adapters.CowboyTest.HTTP,
-               _,
-               _,
-               [env: [dispatch: @dispatch], onresponse: default_response]
-             ] = args(:http, __MODULE__, [], [])
-
-      my_onresponse = fn _, _, _, req -> req end
-
-      assert [
-               Plug.Adapters.CowboyTest.HTTP,
-               _,
-               _,
-               [env: [dispatch: @dispatch], onresponse: on_response]
-             ] = args(:http, __MODULE__, [], protocol_options: [onresponse: my_onresponse])
-
-      assert is_function(on_response)
-      assert on_response != default_response
-      assert on_response != my_onresponse
+    test "proxies if Plug.Cowboy is defined" do
+      assert {:ok, :child_spec} == Plug.Adapters.Cowboy.child_spec([])
     end
+  end
 
-    test "includes the default onresponse handler and handles the user-provided onresponse handler tuple" do
-      # Grab a ref to the default onresponse handler
-      assert [
-               Plug.Adapters.CowboyTest.HTTP,
-               _,
-               _,
-               [env: [dispatch: @dispatch], onresponse: default_response]
-             ] = args(:http, __MODULE__, [], [])
+  defp test_raise(fun) do
+    unload_plug_cowboy()
 
-      my_onresponse = {Plug.Adapters.CowboyTest, :my_onresponse_handler}
+    output =
+      capture_io(:stderr, fn ->
+        assert_raise(RuntimeError, @raise_message, fn ->
+          fun.()
+        end)
+      end)
 
-      assert [
-               Plug.Adapters.CowboyTest.HTTP,
-               _,
-               _,
-               [env: [dispatch: @dispatch], onresponse: on_response]
-             ] = args(:http, __MODULE__, [], protocol_options: [onresponse: my_onresponse])
+    assert output =~ @missing_warning
+  end
 
-      assert is_function(on_response)
-      assert on_response != default_response
-      assert on_response != my_onresponse
-    end
+  defp unload_plug_cowboy() do
+    :code.delete(Plug.Cowboy)
+    :code.purge(Plug.Cowboy)
+    Code.unload_files([@plug_cowboy_path])
   end
 end
