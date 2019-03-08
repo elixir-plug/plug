4ddad23d82c42eaf6c64a51feda307e628b38813
Arkadiusz Gil <arkgil@users.noreply.github.com>
2019-03-08 10:27:51 +0100

    Add a Plug.Telemetry for instrumentation (#824)

---
+++ lib/plug/telemetry.ex
@@ -0,0 +1,100 @@
+defmodule Plug.Telemetry do
+  @moduledoc """
+  A plug to instrument the pipeline with `:telemetry` events.
+
+  Currently the following events are emitted (event names assume that `:event_prefix` option is set
+  to `[:my, :plug]` - more on that in the paragraph below):
+
+    * `[:my, :plug, :call, :start]` - emitted right after this plug is invoked. The event carries
+      a single measurement, `:time`, which is the system time at the moment the event is emitted.
+      The only metadata is the whole `Plug.Conn` under the `:conn` key
+    * `[:my, :plug, :call, :stop]` - emitted right before the request is sent back. The event carries
+      two measurements
+
+        * `:time`, which is the system time at the moment the event is emitted
+        * `:duration`, which is the monotonic time difference between the stop and start events
+
+      The same as for the start event, the only metadata is the `Plug.Conn` struct under the `:conn`
+      key.
+
+  The names of the events are based on the provided `event_prefix`: for the start event, the name is
+  `event_prefix ++ [:call, :start]`, and for the stop event: `event_prefix ++ [:call, :stop]`.
+  The event prefix is required, so that event consumers can differentiate between events emitted by
+  multiple instances of this plug.
+
+  Note that this plug measures only the time between its invocation and the rest of the plug pipeline -
+  this can be used to exclude some plugs from measurement.
+
+  ## Time unit
+
+  Both `:time` and `:duration` measurements are presented in the `:native` time unit. You can read
+  more about it in the docs for `System.convert_time_unit/3`.
+
+  ## Example
+
+      defmodule InstrumentedPlug do
+        use Plug.Router
+
+        plug :match
+
+        plug Plug.Telemetry, event_prefix: [:my, :plug]
+        plug Plug.Parsers, parsers: [:urlencoded, :multipart]
+
+        plug :dispatch
+
+        get "/" do
+          send_resp(conn, 200, "Hello, world!")
+        end
+      end
+
+  In this example, the stop event's `duration` includes the time it takes to parse the request,
+  dispatch it to the correct handler, and execute the handler. The events are not emitted for requests
+  not matching any handlers, since the plug is placed after the match plug.
+  """
+
+  @behaviour Plug
+
+  @impl true
+  def init(opts) do
+    event_prefix = ensure_event_prefix!(opts)
+    ensure_valid_event_prefix!(event_prefix)
+    start_event = event_prefix ++ [:call, :start]
+    stop_event = event_prefix ++ [:call, :stop]
+    {start_event, stop_event}
+  end
+
+  @impl true
+  def call(conn, {start_event, stop_event}) do
+    start_time = System.monotonic_time()
+    :telemetry.execute(start_event, %{time: System.system_time()}, %{conn: conn})
+
+    Plug.Conn.register_before_send(conn, fn conn ->
+      duration = System.monotonic_time() - start_time
+
+      :telemetry.execute(stop_event, %{duration: duration, time: System.system_time()}, %{
+        conn: conn
+      })
+
+      conn
+    end)
+  end
+
+  defp ensure_event_prefix!(opts) do
+    case Keyword.fetch(opts, :event_prefix) do
+      {:ok, event_prefix} ->
+        event_prefix
+
+      _ ->
+        raise ArgumentError, ":event_prefix is required"
+    end
+  end
+
+  defp ensure_valid_event_prefix!(event_prefix) do
+    if is_list(event_prefix) && Enum.all?(event_prefix, &is_atom/1) do
+      :ok
+    else
+      raise ArgumentError,
+            "expected :event_prefix to be a list of atoms, got: #{inspect(event_prefix)}"
+    end
+  end
+end
---
+++ test/plug/telemetry_test.exs
@@ -0,0 +1,128 @@
+defmodule Plug.TelemetryTest do
+  use ExUnit.Case, async: true
+  use Plug.Test
+
+  defmodule MyPlug do
+    use Plug.Builder
+
+    plug Plug.Telemetry, event_prefix: [:pipeline]
+
+    plug :send_resp, 200
+
+    defp send_resp(conn, status) do
+      Plug.Conn.send_resp(conn, status, "Response")
+    end
+  end
+
+  defmodule MyNoSendPlug do
+    use Plug.Builder
+
+    plug Plug.Telemetry, event_prefix: [:nosend, :pipeline]
+  end
+
+  defmodule MyCrashingPlug do
+    use Plug.Builder
+
+    plug Plug.Telemetry, event_prefix: [:crashing, :pipeline]
+    plug :raise_error
+    plug :send_resp, 200
+
+    defp raise_error(_conn, _) do
+      raise "Crash!"
+    end
+
+    defp send_resp(conn, status) do
+      Plug.Conn.send_resp(conn, status, "Response")
+    end
+  end
+
+  setup do
+    start_handler_id = {:start, :rand.uniform(100)}
+    stop_handler_id = {:stop, :rand.uniform(100)}
+
+    on_exit(fn ->
+      :telemetry.detach(start_handler_id)
+      :telemetry.detach(stop_handler_id)
+    end)
+
+    {:ok, start_handler: start_handler_id, stop_handler: stop_handler_id}
+  end
+
+  test "emits an event before the pipeline and before sending the response", %{
+    start_handler: start_handler,
+    stop_handler: stop_handler
+  } do
+    attach(start_handler, [:pipeline, :call, :start])
+    attach(stop_handler, [:pipeline, :call, :stop])
+
+    MyPlug.call(conn(:get, "/"), [])
+
+    assert_received {:event, [:pipeline, :call, :start], measurements, metadata}
+    assert map_size(measurements) == 1
+    assert %{time: time} = measurements
+    assert is_integer(time)
+    assert map_size(metadata) == 1
+    assert %{conn: conn} = metadata
+
+    assert_received {:event, [:pipeline, :call, :stop], measurements, metadata}
+    assert map_size(measurements) == 2
+    assert %{duration: duration, time: time} = measurements
+    assert is_integer(duration)
+    assert is_integer(time)
+    assert map_size(metadata) == 1
+    assert %{conn: conn} = metadata
+    assert conn.state == :set
+    assert conn.status == 200
+  end
+
+  test "doesn't emit a stop event if the response is not sent", %{
+    start_handler: start_handler,
+    stop_handler: stop_handler
+  } do
+    attach(start_handler, [:nosend, :pipeline, :call, :start])
+    attach(stop_handler, [:nosend, :pipeline, :call, :stop])
+
+    MyNoSendPlug.call(conn(:get, "/"), [])
+
+    assert_received {:event, [:nosend, :pipeline, :call, :start], _, _}
+    refute_received {:event, [:nosend, :pipeline, :call, :stop], _, _}
+  end
+
+  test "raises if event prefix is not provided" do
+    assert_raise ArgumentError, ~r/^:event_prefix is required$/, fn ->
+      Plug.Telemetry.init([])
+    end
+  end
+
+  test "raises if event prefix is not a list of atoms" do
+    assert_raise ArgumentError, ~r/^expected :event_prefix to be a list of atoms, got: 1$/, fn ->
+      Plug.Telemetry.init(event_prefix: 1)
+    end
+  end
+
+  test "doesn't emit a stop event when the pipeline crashes", %{
+    start_handler: start_handler,
+    stop_handler: stop_handler
+  } do
+    attach(start_handler, [:crashing, :pipeline, :call, :start])
+    attach(stop_handler, [:crashing, :pipeline, :call, :stop])
+
+    assert_raise RuntimeError, fn ->
+      MyCrashingPlug.call(conn(:get, "/"), [])
+    end
+
+    assert_received {:event, [:crashing, :pipeline, :call, :start], _, _}
+    refute_received {:event, [:crashing, :pipeline, :call, :stop], _, _}
+  end
+
+  defp attach(handler_id, event) do
+    :telemetry.attach(
+      handler_id,
+      event,
+      fn event, measurements, metadata, _ ->
+        send(self(), {:event, event, measurements, metadata})
+      end,
+      nil
+    )
+  end
+end
