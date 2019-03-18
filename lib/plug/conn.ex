e2f873dabe71aee0ca7c0df7b888d6077889f0ed
Awlexus <8594605+Awlexus@users.noreply.github.com>
2019-03-18 10:04:08 +0100

    Update documentation for session_delete/2
    The old description is a bit irritating in my opinion, because it says that it deletes the session, instead of just a value of the session

--- lib/plug/conn.ex
+++ lib/plug/conn.ex
@@ -1431,7 +1431,7 @@ defmodule Plug.Conn do
   end
 
   @doc """
-  Deletes the session for the given `key`.
+  Deletes a `key` from the session.
 
   The key can be a string or an atom, where atoms are
   automatically converted to strings.
