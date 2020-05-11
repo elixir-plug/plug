81bd14d36228d6b7dee35c37071c871fa126f687
Enzo Piacenza <epiacenza@hawk.iit.edu>
2020-05-11 14:53:50 +0200

    Adding option to disable utf-8 validation urlencoded params (#937)

--- lib/plug/parsers/urlencoded.ex
+++ lib/plug/parsers/urlencoded.ex
@@ -28,7 +28,15 @@ defmodule Plug.Parsers.URLENCODED do
   def parse(conn, "application", "x-www-form-urlencoded", _headers, {{mod, fun, args}, opts}) do
     case apply(mod, fun, [conn, opts | args]) do
       {:ok, body, conn} ->
-        {:ok, Plug.Conn.Query.decode(body, %{}, Plug.Parsers.BadEncodingError), conn}
+        validate_utf8 = Keyword.get(opts, :validate_utf8, true)
+
+        {:ok,
+         Plug.Conn.Query.decode(
+           body,
+           %{},
+           Plug.Parsers.BadEncodingError,
+           validate_utf8
+         ), conn}
 
       {:more, _data, conn} ->
         {:error, :too_large, conn}
