197bf5f6ce9049cca906e632b139809a032f5829
Enzo Piacenza <enzo@telnyx.com>
2020-05-11 12:39:56 +0200

    Break code into multiple lines, add tests

--- lib/plug/parsers/urlencoded.ex
+++ lib/plug/parsers/urlencoded.ex
@@ -28,12 +28,14 @@ defmodule Plug.Parsers.URLENCODED do
   def parse(conn, "application", "x-www-form-urlencoded", _headers, {{mod, fun, args}, opts}) do
     case apply(mod, fun, [conn, opts | args]) do
       {:ok, body, conn} ->
+        validate_utf8 = Keyword.get(opts, :validate_utf8, true)
+
         {:ok,
          Plug.Conn.Query.decode(
            body,
            %{},
            Plug.Parsers.BadEncodingError,
-           Keyword.get(opts, :validate_utf8, true)
+           validate_utf8
          ), conn}
 
       {:more, _data, conn} ->
