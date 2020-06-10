01f5c1ebe7f48d0f3023e8ea8ac0187ba3b76e0e
José Valim <jose.valim@dashbit.co>
2020-06-10 19:07:35 +0200

    Release v1.10.3

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule Plug.MixProject do
   use Mix.Project
 
-  @version "1.10.2"
+  @version "1.10.3"
   @description "A specification and conveniences for composable modules between web applications"
   @xref_exclude [Plug.Cowboy, :telemetry]
 
