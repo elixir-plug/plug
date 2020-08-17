7ca5f6aba66eeb87bc39ce63018a430b986feed4
Gergő Papp-Szentannai <gergo_13@me.com>
2020-08-17 16:50:20 +0300

    Include LICENSE file when publishing to hex

--- mix.exs
+++ mix.exs
@@ -53,7 +53,7 @@ defmodule Plug.MixProject do
       licenses: ["Apache-2.0"],
       maintainers: ["Gary Rennie", "José Valim"],
       links: %{"GitHub" => "https://github.com/elixir-plug/plug"},
-      files: ["lib", "mix.exs", "README.md", "CHANGELOG.md", "src", ".formatter.exs"]
+      files: ["lib", "mix.exs", "README.md", "CHANGELOG.md", "LICENSE", "src", ".formatter.exs"]
     }
   end
 
