ff797b6b445e301fcd8b3a0fe845f65a4554cb33
Eksperimental <eksperimental@autistici.org>
2021-01-25 12:56:00 -0500

    Update ExDoc
    It also fixes it as the "docs" task is not currently recognized when run `mix docs`

--- mix.exs
+++ mix.exs
@@ -44,7 +44,7 @@ defmodule Plug.MixProject do
       {:mime, "~> 1.0"},
       {:plug_crypto, "~> 1.1.1 or ~> 1.2"},
       {:telemetry, "~> 0.4"},
-      {:ex_doc, "~> 0.21", only: :docs}
+      {:ex_doc, "~> 0.22.0", only: :dev, runtime: false}
     ]
   end
 
