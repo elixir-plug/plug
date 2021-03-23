81bc409ff0b26a752f027d51086c1e3c990b1e5c
Victor Oliveira Nascimento <victor.olinasc@gmail.com>
2021-03-23 20:23:02 -0300

    Proper assigns type declaration

--- lib/plug/error_handler.ex
+++ lib/plug/error_handler.ex
@@ -48,7 +48,11 @@ defmodule Plug.ErrorHandler do
 
   Called when an exception is raised during the processing of a plug.
   """
-  @callback handle_errors(Plug.Conn.t(), map()) :: no_return()
+  @callback handle_errors(Plug.Conn.t(), %{
+              type: :error | :throw | :exit,
+              reason: Exception.t() | term(),
+              stacktrace: Exception.stacktrace()
+            }) :: no_return()
 
   @doc false
   defmacro __using__(_) do
