0e3fcdef21822247a0ae6f9437f9237c445ad96e
Leandro Cesquini Pereira <leandro@leandro.io>
2023-01-06 16:14:37 -0500

    Leverage https://github.com/elixir-lang/elixir/pull/12322 to pretty print KeyError message, and improve layout styling.

--- lib/plug/debugger.ex
+++ lib/plug/debugger.ex
@@ -186,7 +186,7 @@ defmodule Plug.Debugger do
   def render(conn, status, kind, reason, stack, opts) do
     session = maybe_fetch_session(conn)
     params = maybe_fetch_query_params(conn)
-    {title, message, pretty} = info(kind, reason)
+    {title, message} = info(kind, reason)
     [headline | details] = String.split(message, "\n\n")
 
     assigns = [
@@ -216,7 +216,6 @@ defmodule Plug.Debugger do
           conn: conn,
           headline: headline,
           details: details,
-          pretty: pretty,
           markdown: markdown,
           style: style,
           banner: banner,
@@ -307,15 +306,9 @@ defmodule Plug.Debugger do
   defp status(:error, error), do: Plug.Exception.status(error)
   defp status(_, _), do: 500
 
-  defp info(:error, %KeyError{key: key, term: term} = error) when not is_nil(term) do
-    message = "key #{inspect(key)} not found in: %" <> inspect(term.__struct__) <> "{...}"
-    pretty = inspect(term, pretty: true, limit: :infinity)
-    {inspect(error.__struct__), message, pretty}
-  end
-
-  defp info(:error, error), do: {inspect(error.__struct__), Exception.message(error), nil}
-  defp info(:throw, thrown), do: {"unhandled throw", inspect(thrown), nil}
-  defp info(:exit, reason), do: {"unhandled exit", Exception.format_exit(reason), nil}
+  defp info(:error, error), do: {inspect(error.__struct__), Exception.message(error)}
+  defp info(:throw, thrown), do: {"unhandled throw", inspect(thrown)}
+  defp info(:exit, reason), do: {"unhandled exit", Exception.format_exit(reason)}
 
   defp frames(renderer, stacktrace, opts) do
     app = opts[:otp_app]
