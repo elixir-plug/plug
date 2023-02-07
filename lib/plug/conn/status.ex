69cb834802a853a4b461b8bde288a5b54cd43ca2
Jonathan Clem <j@jclem.me>
2023-02-07 11:34:02 -0500

    Fix Plug.Conn.Status.reason_phrase/1 error message (#1131)
    The message referenced the atom `:unavailable_for_legal_reasons`,
    but should read `:not_an_rfc_status_code`.

--- lib/plug/conn/status.ex
+++ lib/plug/conn/status.ex
@@ -166,7 +166,7 @@ defmodule Plug.Conn.Status do
         MIX_ENV=dev mix deps.clean plug --build
 
     Doing this will allow the use of the integer status code 998 as
-    well as the atom :unavailable_for_legal_reasons in many Plug functions.
+    well as the atom :not_an_rfc_status_code in many Plug functions.
     For example:
 
         put_status(conn, :not_an_rfc_status_code)
