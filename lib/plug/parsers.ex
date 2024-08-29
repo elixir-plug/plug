d8c17361f16530acce75edc157a8c107fd883a63
Tony Dang <tony@tonydang.blog>
2024-08-29 14:18:09 -0700

    improve custom body reader example (#1243)

--- lib/plug/parsers.ex
+++ lib/plug/parsers.ex
@@ -192,9 +192,10 @@ defmodule Plug.Parsers do
 
       defmodule CacheBodyReader do
         def read_body(conn, opts) do
-          {:ok, body, conn} = Plug.Conn.read_body(conn, opts)
-          conn = update_in(conn.assigns[:raw_body], &[body | (&1 || [])])
-          {:ok, body, conn}
+          with {:ok, body, conn} <- Plug.Conn.read_body(conn, opts) do
+            conn = update_in(conn.assigns[:raw_body], &[body | &1 || []])
+            {:ok, body, conn}
+          end
         end
       end
 
