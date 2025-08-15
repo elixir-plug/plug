1029c62e74eec26b639cef073efcec67614ac918
José Valim <jose.valim@dashbit.co>
2025-08-15 17:05:16 +0200

    Add tests for give_away/3

--- test/plug/upload_test.exs
+++ test/plug/upload_test.exs
@@ -39,7 +39,7 @@ defmodule Plug.UploadTest do
     refute File.exists?(path)
   end
 
-  test "give_away/3 assigns ownership to other pid" do
+  test "give_away/2 assigns ownership to other pid" do
     parent = self()
 
     {other_pid, other_ref} =
@@ -87,6 +87,74 @@ defmodule Plug.UploadTest do
       after
         1_000 -> flunk("didn't get a path")
       end
+
+    receive do
+      {:DOWN, ^ref, :process, ^pid, :normal} ->
+        wait_until(fn -> not File.exists?(path3) end)
+        assert File.exists?(path1)
+        assert File.exists?(path2)
+    end
+
+    send(other_pid, :exit)
+
+    receive do
+      {:DOWN, ^other_ref, :process, ^other_pid, :normal} ->
+        wait_until(fn -> not File.exists?(path1) end)
+        wait_until(fn -> not File.exists?(path2) end)
+    end
+  end
+
+  test "give_away/3 assigns ownership to other pid fro third party pid" do
+    parent = self()
+
+    {other_pid, other_ref} =
+      spawn_monitor(fn ->
+        receive do
+          :exit -> nil
+        end
+      end)
+
+    {pid, ref} =
+      spawn_monitor(fn ->
+        {:ok, path1} = Plug.Upload.random_file("sample")
+        send(parent, {:path1, path1})
+        File.open!(path1)
+
+        {:ok, path2} = Plug.Upload.random_file("sample")
+        send(parent, {:path2, path2})
+        File.open!(path2)
+
+        {:ok, path3} = Plug.Upload.random_file("sample")
+        send(parent, {:path3, path3})
+        File.open!(path3)
+
+        assert_receive :done
+      end)
+
+    path1 =
+      receive do
+        {:path1, path} -> path
+      after
+        1_000 -> flunk("didn't get a path")
+      end
+
+    path2 =
+      receive do
+        {:path2, path} -> path
+      after
+        1_000 -> flunk("didn't get a path")
+      end
+
+    path3 =
+      receive do
+        {:path3, path} -> path
+      after
+        1_000 -> flunk("didn't get a path")
+      end
+
+    :ok = Plug.Upload.give_away(path1, other_pid, pid)
+    :ok = Plug.Upload.give_away(path2, other_pid, pid)
+    send(pid, :done)
 
     receive do
       {:DOWN, ^ref, :process, ^pid, :normal} ->
