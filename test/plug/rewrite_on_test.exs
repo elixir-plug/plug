437a2d93204b88d09c6bae40c196eb1b2d71bac0
José Valim <jose.valim@gmail.com>
2026-06-22 13:12:00 +0200

    Harden integer parsing around headers

--- test/plug/rewrite_on_test.exs
+++ test/plug/rewrite_on_test.exs
@@ -74,6 +74,15 @@ defmodule Plug.RewriteOnTest do
     assert conn.port == 3030
   end
 
+  test "does not rewrite port with more than 5 bytes" do
+    conn =
+      conn(:get, "http://example.com/")
+      |> put_req_header("x-forwarded-port", "303030")
+      |> call(:x_forwarded_port)
+
+    assert conn.port == 80
+  end
+
   test "rewrites port with a custom header" do
     conn =
       conn(:get, "http://example.com/")
