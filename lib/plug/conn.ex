129fa9f3e7e2ddd6cd0fbb2de947f67ecbc42213
Victor Rodrigues <vitumail@gmail.com>
2026-06-23 14:50:52 +0100

    Allow atom header keys in inform/3

--- lib/plug/conn.ex
+++ lib/plug/conn.ex
@@ -1437,7 +1437,7 @@ defmodule Plug.Conn do
 
   defp adapter_inform(%Conn{adapter: {adapter, payload}}, status, headers) do
     for {key, value} <- headers do
-      validate_header_key_value!(key, value)
+      validate_header_key_value!(to_string(key), value)
     end
 
     adapter.inform(payload, status, headers)
