f1a5ada8faa728aa488f24646a6659d024f98df3
José Valim <jose.valim@gmail.com>
2026-06-30 14:59:02 +0200

    Release v1.19.4

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,11 @@
 # Changelog
 
+## v1.19.4 (2026-06-30)
+
+### Bug fixes
+
+  * [Plug.Conn] Allow atom header keys on `inform`
+
 ## v1.19.3 (2026-06-23)
 
 ### Security
--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule Plug.MixProject do
   use Mix.Project
 
-  @version "1.19.3"
+  @version "1.19.4"
   @description "Compose web applications with functions"
   @xref_exclude [Plug.Cowboy, :ssl]
   @source_url "https://github.com/elixir-plug/plug"
